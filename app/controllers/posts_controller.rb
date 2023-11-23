a0012a48bd765414a7d7c9c2013d52a161299cfc
Emirali Caferzade <ecaferzade@hotmail.com>
2023-11-23 13:43:14 +0100

    now it is possible to create a new blog post in the db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
 
     def index
     end
+
+    def new
+    end
+
+    def create
+        @post = Post.new(post_params)
+    
+        if @post.save
+          redirect_to root_path, notice: 'Post was successfully created.'
+        else
+          render :new
+        end
+    end
+    
+    private
+    def post_params
+        params.permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+
+  get "posts", to: "posts#new"
+
+  post "posts", to: "posts#create"
 end
